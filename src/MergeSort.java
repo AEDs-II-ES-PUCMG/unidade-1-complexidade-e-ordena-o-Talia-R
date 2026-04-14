8c70d88c1bca9cc9f1e33b7fe54902ce940d649d
Talia-R <natalia.1067@hotmail.com>
2026-04-13 21:09:06 -0300

    refactor

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,92 +1,96 @@
+import java.util.Arrays;
+
 public class MergeSort<T extends Comparable<T>> implements IOrdenador<T>{
-	/**
-	* Algoritmo de ordenação Mergesort.
-	* @param int esq: início do array a ser ordenado
-	* @param int dir: fim do array a ser ordenado
-	*/
-	// 1.a chamada do método mergesort: esq: 0; dir: array.length - 1
-	
-	/* vira ordenar
-	private void mergesort(int[] array, int esq, int dir) {
-		if (esq < dir) {
-				int meio = (esq + dir) / 2;
-				mergesort(array, esq, meio);
-				mergesort(array, meio + 1, dir);
-				intercalar(array, esq, meio, dir);
-			}
-	}
-	*/
+	private long comparacoes;
+    private long movimentacoes;
+    private double tempoOrdenacao;
+    private double inicio;
+
+    private double nanoToMilli = 1.0/1_000_000;
+
+    private void iniciar(){
+        this.comparacoes = 0;
+        this.movimentacoes = 0;
+        this.inicio = System.nanoTime();
+    }
+
+    private void terminar(){
+        this.tempoOrdenacao = (System.nanoTime() - this.inicio) * nanoToMilli;
+    }
 
 	/**
 	* Algoritmo que intercala os elementos localizados entre as posições esq e dir
 	* @param int esq: início do array a ser ordenado
 	* @param int meio: posição do meio do array a ser ordenado
 	* @param int dir: fim do array a ser ordenado
-	*/ 
-	private void intercalar(T[] dados, int esq, int meio, int dir) {
+	*/
+    private void intercalar(T[] dados, int esq, int meio, int dir) {
 
-		int n1, n2, i, j, k;
+        int n1 = meio - esq + 1;
+        int n2 = dir - meio;
 
-			//Definir tamanho dos dois subarrays
-			n1 = meio - esq + 1;
-			n2 = dir - meio;
+        T[] a1 = Arrays.copyOfRange(dados, esq, meio + 1);
+        T[] a2 = Arrays.copyOfRange(dados, meio + 1, dir + 1);
 
-			int[] a1 = new int[n1];
-			int[] a2 = new int[n2];
+        int i = 0, j = 0, k = esq;
 
-			//Inicializar primeiro subarray
-			for (i = 0; i < n1; i++) {
-				a1[i] = array[esq + i];
-			}
+        while (i < n1 && j < n2) {
+            comparacoes++;
+            if (a1[i].compareTo(a2[j]) <= 0) {
+                dados[k++] = a1[i++];
+            } else {
+                dados[k++] = a2[j++];
+            }
+            movimentacoes++;
+        }
 
-			//Inicializar segundo subarray
-			for (j = 0; j < n2; j++) {
-				a2[j] = array[meio + j + 1];
-			}
+        while (i < n1) {
+            dados[k++] = a1[i++];
+            movimentacoes++;
+        }
 
-		//Intercalação propriamente dita
-			for (i = j = 0, k = esq; (i < n1 && j < n2); k++) {
-				if (a1[i] <= a2[j])
-				array[k] = a1[i++];
-			else
-				array[k] = a2[j++];
-			}
-		
-		if (i == n1)
-			for (; k <= dir; k++) {
-					array[k] = a2[j++];
-				}
-		else
-				for (; k <= dir; k++) {
-					array[k] = a1[i++];
-				}
-	}
+        while (j < n2) {
+            dados[k++] = a2[j++];
+            movimentacoes++;
+        }
+    }
 
-	@Override
-	public T[] ordenar(T[] dados, int esq, int dir) {
-		if (esq < dir) {
-				int meio = (esq + dir) / 2;
-				ordenar(dados, esq, meio);
-				ordenar(dados, meio + 1, dir);
-				intercalar(dados, esq, meio, dir);
-			}
-	}
+	/**
+	* Algoritmo de ordenação Mergesort.
+	* @param int esq: início do array a ser ordenado
+	* @param int dir: fim do array a ser ordenado
+	*/
+	private void mergesort(T[] dados, int esq, int dir) {
+        if (esq < dir) {
+            int meio = (esq + dir) / 2;
+            mergesort(dados, esq, meio);
+            mergesort(dados, meio + 1, dir);
+            intercalar(dados, esq, meio, dir);
+        }
+    }
+
+	
+    @Override
+    public T[] ordenar(T[] dados) {
+        T[] dadosOrdenados = Arrays.copyOf(dados, dados.length);
+        iniciar();
+        mergesort(dadosOrdenados, 0, dadosOrdenados.length - 1);
+        terminar();
+        return dadosOrdenados;
+    }
 
 	@Override
 	public long getComparacoes() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getComparacoes'");
+		return comparacoes;
 	}
 
 	@Override
 	public long getMovimentacoes() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getMovimentacoes'");
+		return movimentacoes;
 	}
 
 	@Override
 	public double getTempoOrdenacao() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getTempoOrdenacao'");
+		return tempoOrdenacao;
 	}
 }
